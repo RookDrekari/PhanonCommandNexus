f21e059b6197c9731f47806d87eabaea8c2d46c5
Rook <rookdrekari@proton.me>
2025-04-03 20:35:07 -0500

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-cd /home/rook/PCN
+cd /home/rook/PCN/pcn || exit 1
 
-git pull origin main
+git pull origin main || exit 1
 
-cd pcn
-
-hugo
-sudo chown -R rook:www-data /home/rook/PCN/pcn/public
-sudo chmod -R 755 /home/rook/PCN/pcn/public
-sudo systemctl restart nginx
+hugo || exit 1
+sudo chown -R rook:www-data /home/rook/PCN/pcn/public || exit 1
+sudo chmod -R 755 /home/rook/PCN/pcn/public || exit 1
+sudo systemctl restart nginx || exit 1
 
 echo "Deployment complete!"
